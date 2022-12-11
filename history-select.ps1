ed4645a5437f8e16dee3f0eb13f668324ea44cf2
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2022-12-11 13:51:24 +0100

    use pipeline

--- history-select.ps1
+++ history-select.ps1
@@ -1,9 +1,9 @@
 param([string]$p1)
 if($p1) {
-    $Obj = Get-History | Where-Object{$_.CommandLine -Match $p1} 
-    $Obj | Select-Object Id, CommandLine
+    Get-History | Where-Object{$_.CommandLine -Match $p1} |
+        Tee-Object -Variable Obj | 
+        Select-Object Id, CommandLine
     "`n{0} total commands in history" -f $Obj.Count
 } else {
-    Write-Host "Usage:`n`t{0} <string-to-find>" -f $MyInvocation.MyCommand.Name 
-    
+    Write-Host "Usage:`n`t{0} <string-to-find>" -f $MyInvocation.MyCommand.Name   
 }
