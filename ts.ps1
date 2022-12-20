6486f1eea6b181306343a2154ac858f76ef1fa45
Adrian Biro <biroadrian@protonmail.com>
2022-12-20 10:46:29 +0100

    delta from pipeline

--- ts.ps1
+++ ts.ps1
@@ -16,13 +16,43 @@ param(
     [switch] $iso,
     [switch] $time,
     [switch] $date,
-    [switch] $date_time
+    [switch] $date_time,
+    [switch] $delta_stdin
 )
-$tform = if($tz){ "yyyy-MM-dd HH:mm:ss.ffff K" }
-elseif ($iso) { "o" }
-elseif ($time) {"HH:mm:ss"}
-elseif ($date) {"yyyy-MM-dd"}
-elseif ($date_time) {"yyyy-MM-dd HH:mm:ss"}
-else { "yyyy-MM-dd HH:mm:ss.ffff" }
+function get-timestamp {
+    $tform = if($tz){ "yyyy-MM-dd HH:mm:ss.ffff K" }
+        elseif ($iso) { "o" }
+        elseif ($time) {"HH:mm:ss"}
+        elseif ($date) {"yyyy-MM-dd"}
+        elseif ($date_time) {"yyyy-MM-dd HH:mm:ss"}
+        else { "yyyy-MM-dd HH:mm:ss.ffff" }
 
-get-date -Format $tform
+    return (get-date -Format $tform)
+}
+function get-delta-from-pipeline {
+    [cmdletbinding()]
+    param(
+        [parameter(
+            Mandatory         = $true,
+            ValueFromPipeline = $true)]
+        $pipelineInput
+    )
+    
+        Begin { }
+        Process {}
+        End {
+            $StartTime = get-date
+            ForEach ($i in $pipelineInput) {
+                # FIXME
+                "{0} {1}" -f $i, (NEW-TIMESPAN –Start $StartTime –End ($EndTime = Get-Date)).TotalSeconds
+                $StartTime -lt $endtime
+                $StartTime = $EndTime
+            }
+        }
+        #End { }
+}
+if($delta_stdin) { 
+    get-delta-from-pipeline $input
+} else {
+    get-timestamp
+}
