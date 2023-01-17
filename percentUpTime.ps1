80670bff02275d2b9a949794782eb69a73f21216
Adrian Biro <biroadrian@protonmail.com>
2023-01-17 12:26:47 +0100

    fix encoding in profile

---
+++ percentUpTime.ps1
@@ -0,0 +1,10 @@
+Param(
+    [int] $Month,
+    [double] $DownTimeSec
+
+)
+$firstDay = Get-Date -Day 1 -Hour 0 -Minute 0 -Second 0 -Month $Month
+$lastDay = ($firstDay).AddMonths(1).AddSeconds(-1)
+$SecondsInMonth = (3600 * 24) * $lastDay.Day 
+$PercentUp = (($SecondsInMonth - $DownTimeSec) / $SecondsInMonth) * 100
+Write-Host ("{0:f6}% Uptime for {1} month" -f $PercentUp, $Month)
