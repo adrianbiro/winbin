fade0e66f1106d1146dfd15feb5d9a30522b0194
Adrian Biro <biroadrian@protonmail.com>
2023-01-21 09:24:27 +0100

    cleanup

--- help.ps1
+++ help.ps1
@@ -1,4 +1,4 @@
-$exclude = "*.md", "Microsoft.PowerShell_profile.ps1", $MyInvocation.MyCommand.Name, "dotnet-disable-tel.ps1"
+$exclude = "*.md", "Microsoft.PowerShell_profile.ps1", $MyInvocation.MyCommand.Name, ".gitignore", "zmaz", "settings"
 
 Get-ChildItem (Join-Path -Path $HOME -ChildPath "src\winbin") -Exclude $exclude
     | Format-Wide -Column 3 -Property Name
