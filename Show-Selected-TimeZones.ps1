9b8bfd3aba62417a00ef33856d10b5c8d9e957ce
Adrian Biro <biroadrian@protonmail.com>
2023-11-15 11:08:24 +0100

    small fixes

--- Show-Selected-TimeZones.ps1
+++ Show-Selected-TimeZones.ps1
@@ -1,13 +1,24 @@
 <#
     .SYNOPSIS
         Show Selected Time Zones.
-    .DESCRIPTION  
+    .DESCRIPTION
+        Show current time:
+            .\Show-Selected-TimeZones.ps1 
+        Show Custom time or date and time:
+            .\Show-Selected-TimeZones.ps1 -Time '20:30:12'
+            .\Show-Selected-TimeZones.ps1 -Time '12.05.2012 20:30:12'
     .NOTES
-        Show Available Time Zones
-        Get-TimeZone -ListAvailable | select -Property ID,BaseUtcOffset
+        Central Europe Standard Time is assumed to be default machine configuration.
+        If no time but the date is specified, the default time will be 00:00:00.  
+        Show all Available Time Zones:
+            Get-TimeZone -ListAvailable | select -Property ID,BaseUtcOffset
     .LINK
         https://github.com/adrianbiro/winbin
 #>
+Param(
+    [Parameter(Mandatory = $false)]
+    [datetime]$Time = (Get-Date)
+)
 [string[]]$Zones = @(
     'Pacific Standard Time', 
     'Mountain Standard Time', 
@@ -26,7 +37,7 @@ $Obj = @()
 
 foreach ($i in $Zones) { 
     $Obj += New-Object psobject -Property @{ 
-        DateTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($(Get-Date), $i)
+        DateTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($Time, $i)
         TimeZone = $i
 
     }
