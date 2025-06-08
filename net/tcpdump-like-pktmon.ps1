1c24412f4de36cdcc46e0975d0c88de5038a0360
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2025-06-08 15:07:46 +0200

    Create tcpdump-like-pktmon.ps1

---
+++ net/tcpdump-like-pktmon.ps1
@@ -0,0 +1,3 @@
+#https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/pktmon
+#https://learn.microsoft.com/en-us/windows-server/networking/technologies/pktmon/pktmon
+pktmon start --capture --log-mode real-time
