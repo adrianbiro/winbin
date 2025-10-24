c2b995a9c00ebf6980b76d91671dcf5e4b612a41
Adrian Biro <biroadrian@protonmail.com>
2025-10-24 12:50:30 +0200

    small fixes

--- install/vm-bypass.bat
+++ install/vm-bypass.bat
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ridercz/Scripts/refs/heads/master/Setup-Windows11/OOBE/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/adrianbiro/winbin/refs/heads/main/install/vm-unattend.xml
 C:\Windows\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
